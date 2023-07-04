c5816a196428c046c2d30a5e705327e37a683beb
harithamv <parvanhrahul@gmail.com>
2023-07-04 06:39:35 +0530

    R2 commit

---
+++ store/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+
+def store(request):
+    context = {}
+    return render(request, 'store.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'checkout.html', context)
